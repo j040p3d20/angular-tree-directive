b78ad3337563a1ae8df7756963f40f37e6a0065d
j040p3d20 <j040p3d20@gmail.com>
2016-03-12 12:17:50 +0000

    initial version

---
+++ angular-tree-table.js
@@ -0,0 +1,76 @@
+
+angular
+.module( 'angularTreeTable' , [] )
+.directive( 'angularTreeTable' , function ( $compile , $timeout ) {
+	return {
+		scope : {
+			rows : '=',
+			columns : '='
+		},
+		transclude : true,
+		link : function( scope , element , attrs , controller , transcludeFn ){
+			
+			scope.$watch('rows', function( rows ) {
+				element.empty();
+				scope.open = true;
+				appendRows( element , scope , rows );
+			});
+			
+			function appendRows( element , scope , rows )
+			{
+				if ( rows && rows.length )
+				{
+					for ( var i = 0 ; i < rows.length ; i++ )
+					{
+						var row = rows[i];
+						appendRow( element, scope.$new() , row );
+					}
+				}
+			}
+			
+			function appendRow( element, scope, row )
+			{
+				scope.row = row;
+				scope.open = false;
+				scope.depth = ( scope.depth == undefined ? 0 : 1 + scope.$parent.depth );
+				
+				scope.toggle = function(){
+					var open = ! this.open;
+					$timeout(function(){
+						if ( open )
+						{
+							scope.open = open;
+						}
+						else
+						{
+							close( scope );
+						}
+					},0);
+				}
+				
+				transcludeFn( scope, function( clone , scope ){
+					element.append( $compile( clone )( scope ) );
+				} );
+				
+				if ( row.rows )
+				{
+					appendRows( element , scope , row.rows );
+				}
+			}
+			
+			function close( scope )
+			{
+				if ( scope.$$childHead )
+				{
+					for ( var child = scope.$$childHead ; child != null ; child = child.$$nextSibling )
+					{
+						close( child );
+					}
+				}
+				scope.open = false;
+			}
+			
+		}
+	}
+})
+;
